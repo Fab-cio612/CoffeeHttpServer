ca0d6247705fd6b79af90eb050d57e90202f51fe
Fabio <f.cioni612@gmail.com>
2020-12-06 15:27:44 +0100

    FirstCommit

---
+++ my-app/src/main/java/com/fab_cio612/CoffeeHttpServer/App.java
@@ -0,0 +1,13 @@
+package com.fab_cio612.CoffeeHttpServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
